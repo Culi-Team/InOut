72b176abce02bc32dd3fc44baf0eedc81debe5d5
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-07-25 19:55:12 +0700

    Update PickSequence

--- EQX.InOut/Cylinder/SimulationCylinder.cs
+++ EQX.InOut/Cylinder/SimulationCylinder.cs
@@ -4,6 +4,29 @@ using System.Reflection;
 
 namespace EQX.InOut
 {
+    public static class SimulationInputSetter
+    {
+        public static void SetSimInput(IDInput? input, bool value)
+        {
+            if (input == null) return;
+
+            using (MemoryMappedViewStream stream = _mmf.CreateViewStream(input.Id, 0))
+            {
+                BinaryWriter writer = new BinaryWriter(stream);
+                if (value)
+                {
+                    writer.Write((char)1);
+                }
+                else
+                {
+                    writer.Write((char)0);
+                }
+            }
+        }
+
+        static MemoryMappedFile _mmf = MemoryMappedFile.OpenExisting("SimInputData");
+    }
+
     public class SimulationCylinder : CylinderBase
     {
         #region Constructors
