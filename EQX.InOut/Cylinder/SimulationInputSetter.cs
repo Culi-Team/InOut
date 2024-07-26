298cc75bb4f5316e055d486ff07a23dae8b242d8
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2024-07-26 10:00:41 +0700

    Update SimulationInputSetter

---
+++ EQX.InOut/Cylinder/SimulationInputSetter.cs
@@ -0,0 +1,28 @@
+﻿using EQX.Core.InOut;
+using System.IO.MemoryMappedFiles;
+
+namespace EQX.InOut
+{
+    public static class SimulationInputSetter
+    {
+        public static void SetSimInput(IDInput? input, bool value)
+        {
+            if (input == null) return;
+
+            using (MemoryMappedViewStream stream = _mmf.CreateViewStream(input.Id, 0))
+            {
+                BinaryWriter writer = new BinaryWriter(stream);
+                if (value)
+                {
+                    writer.Write((char)1);
+                }
+                else
+                {
+                    writer.Write((char)0);
+                }
+            }
+        }
+
+        static MemoryMappedFile _mmf = MemoryMappedFile.OpenExisting("SimInputData");
+    }
+}
