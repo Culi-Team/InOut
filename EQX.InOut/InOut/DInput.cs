77ac31894545698dc0a3dce236129561a904756e
황아잉 (LE HOANG ANH) <hoanganh372001@gmail.com>
2024-09-14 13:54:36 +0700

    Update DInput.cs - PropertyChaged event
    Update PropertyChaged event:
    - The "PropertyChanged" event is only executed when the value of the "Value" property after being updated is different from the value before update.

--- EQX.InOut/InOut/DInput.cs
+++ EQX.InOut/InOut/DInput.cs
@@ -5,28 +5,12 @@ namespace EQX.InOut
 {
     public class DInput : ObservableObject, IDInput
     {
-        private bool _oldValue;
-        private bool _currentValue;
         public event EventHandler ValueUpdated;
         public event EventHandler ValueChanged;
         public int Id { get; init; }
         public string Name { get; init; }
-        public bool Value
-        {
-            get
-            {
+        public bool Value => _dInputDevice[Id];
 
-                _oldValue = _currentValue;
-                _currentValue = _dInputDevice[Id];
-
-                if (_oldValue != _currentValue)
-                {
-                    ValueChanged?.Invoke(this, EventArgs.Empty);
-                }
-
-                return _currentValue;
-            }
-        }
         public DInput(int id, string name, IDInputDevice dInputDevice)
         {
             Id = id;
@@ -39,8 +23,18 @@ namespace EQX.InOut
         {
             OnPropertyChanged("Value");
             ValueUpdated?.Invoke(this, EventArgs.Empty);
+
+            _oldValue = _currentValue;
+            _currentValue = Value;
+
+            if (_oldValue != _currentValue)
+            {
+                ValueChanged?.Invoke(this, EventArgs.Empty);
+            }
         }
 
+        private bool _oldValue;
+        private bool _currentValue;
         private readonly IDInputDevice _dInputDevice;
     }
 }
