c433cd814cfa65b629b01fd1f36b9dcc2285feee
hieu202 <hieubvc159@gmail.com>
2025-06-14 09:26:25 +0700

    Add SimulationInput Modbus

--- EQX.InOut/InOut/SimulationInputDevice.cs
+++ EQX.InOut/InOut/SimulationInputDevice.cs
@@ -1,53 +1,80 @@
-﻿using System.IO.MemoryMappedFiles;
+﻿using FluentModbus;
+using System.IO.MemoryMappedFiles;
+using static log4net.Appender.FileAppender;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace EQX.InOut
 #pragma warning restore IDE0130 // Namespace does not match folder structure
 {
+    public class SimulationInputDeviceServer<TEnum> : InputDeviceBase<TEnum> where TEnum : Enum
+    {
+        private readonly ModbusTcpServer _server;
+        private readonly object _lock = new object();
+
+        public SimulationInputDeviceServer()
+            : base()
+        {
+            _server = new ModbusTcpServer();
+        }
+
+        ~SimulationInputDeviceServer()
+        {
+            _server.Dispose();
+        }
+        public bool this[int index]
+        {
+            get => GetValue(index);
+            set => SetValue(index, value);
+        }
+        public void SetValue(int index, bool value)
+        {
+            _server.GetCoils().Set(index, value);
+        }
+        public bool GetValue(int index)
+        {
+            return (_server.GetCoils()[index / 8] & (1 << index % 8)) != 0;
+        }
+        public void Start()
+        {
+            _server.Start();
+        }
+        private int _offset;
+    }
     public class SimulationInputDevice<TEnum> : InputDeviceBase<TEnum> where TEnum : Enum
     {
-        readonly MemoryMappedFile mmf;
+        private readonly ModbusTcpClient _client;
+        private readonly object _lock = new object();
 
         public SimulationInputDevice()
             : base()
         {
-            mmf = MemoryMappedFile.CreateOrOpen("SimInputData", 256);
-        }
+            _client = new ModbusTcpClient();
+            _client.Connect();
 
+        }
+        public bool this[int index] => ActualGetInput(index);
         ~SimulationInputDevice()
         {
-            mmf.Dispose();
+            _client.Dispose();
         }
+        public void Connect()
+        {
+            try
+            {
+                _client.Connect();
+            }
+            catch (Exception ex)
+            {
 
+            }
+        }
         protected override bool ActualGetInput(int index)
         {
-            byte[] values;
-
-            using (MemoryMappedViewStream stream = mmf.CreateViewStream())
+            lock (_lock)
             {
-                BinaryReader reader = new BinaryReader(stream);
-                values = reader.ReadBytes(256);
-            }
-
-            return values[_offset + index] == 1;
-        }
-
-        public void SetInput(int index, bool value)
-        {
-            using (MemoryMappedViewStream stream = mmf.CreateViewStream(_offset + index, 0))
-            {
-                BinaryWriter writer = new BinaryWriter(stream);
-                if (value)
-                {
-                    writer.Write((char)1);
-                }
-                else
-                {
-                    writer.Write((char)0);
-                }
+                var result = _client.ReadCoils(0, index, 1);
+                return (result[0] & 0x01) != 0;
             }
         }
-
-        private int _offset;
     }
 }
