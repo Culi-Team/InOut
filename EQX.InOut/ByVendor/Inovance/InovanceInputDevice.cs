e5c7a809aad510160936f1538cb48a4bc192ed9d
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-08-25 14:00:26 +0700

    Add IO Inovance Device

---
+++ EQX.InOut/ByVendor/Inovance/InovanceInputDevice.cs
@@ -0,0 +1,32 @@
+﻿using EQX.Core.Motion;
+using EQX.Motion.ByVendor.Inovance;
+using Inovance.InoMotionCotrollerShop.InoServiceContract.EtherCATConfigApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.InOut.ByVendor.Inovance
+{
+    public class InovanceInputDevice<TEnum> : InputDeviceBase<TEnum> where TEnum : Enum
+    {
+        public MotionControllerInovance MotionController { get; init; }
+
+        public override bool IsConnected { get => MotionController.IsConnected; }
+
+        public InovanceInputDevice()
+        {
+        }
+
+        protected override bool ActualGetInput(int index)
+        {
+            if (MotionController is null) return false;
+
+            short sValue = 0;
+            ImcApi.IMC_GetEcatDiBit(MotionController.CardHandle, (short)index, ref sValue);
+
+            return sValue == 1;
+        }
+    }
+}
---
+++ EQX.InOut/ByVendor/Inovance/InovanceOutputDevice.cs
@@ -0,0 +1,33 @@
+﻿using EQX.Motion.ByVendor.Inovance;
+using Inovance.InoMotionCotrollerShop.InoServiceContract.EtherCATConfigApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.InOut.ByVendor.Inovance
+{
+    public class InovanceOutputDevice<TEnum> : OutputDeviceBase<TEnum> where TEnum : Enum
+    {
+        public MotionControllerInovance MotionController { get; init; }
+        public override bool IsConnected { get => MotionController.IsConnected; }
+
+        public InovanceOutputDevice()
+        {
+        }
+
+        protected override bool GetOutput(int index)
+        {
+            short sValue = 0;
+            ImcApi.IMC_GetEcatDoBit(MotionController.CardHandle, (short)index, ref sValue);
+
+            return sValue == 1;
+        }
+
+        protected override bool SetOutput(int index, bool value)
+        {
+            return ImcApi.IMC_SetEcatDoBit(MotionController.CardHandle, (short)index, (short)(value ? 1 : 0)) == ImcApi.EXE_SUCCESS;
+        }
+    }
+}
