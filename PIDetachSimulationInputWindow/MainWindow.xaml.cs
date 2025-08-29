61e7830ad1867e33d7c1d9ad442d49c4aea3ea57
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-08-29 16:13:03 +0700

    Add PIDetachSimulationInputWindow

---
+++ PIDetachSimulationInputWindow/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PIDetachSimulationInputWindow;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PIDetachSimulationInputWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new PIDetachSimulationInputWindow.MainWindowViewModel();
+            comboMachineType.ItemsSource = new List<EMachineType> { EMachineType.Tray2CST, EMachineType.CST2CST };
+        }
+    }
+}
