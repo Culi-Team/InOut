85aafff1019c748113320efbad89b838b6edbada
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-08-30 10:29:04 +0700

    Merge

--- EQX.InOut/InOut/SimulationInputDevice_Client.cs
+++ EQX.InOut/InOut/SimulationInputDevice_Client.cs
@@ -48,15 +48,18 @@ namespace EQX.InOut
 
     public class SimulationInputDevice_Client<TEnum> : InputDeviceBase<TEnum> where TEnum : Enum
     {
+        //readonly ModbusTcpClient client;
         MemoryMappedFile? _memoryMapFile;
 
         public SimulationInputDevice_Client()
             : base()
         {
+            //client = new ModbusTcpClient();
         }
 
         ~SimulationInputDevice_Client()
         {
+            //client.Dispose();
             _memoryMapFile.Dispose();
         }
 
@@ -64,6 +67,9 @@ namespace EQX.InOut
         {
             try
             {
+                //client.Connect(new IPEndPoint(IPAddress.Loopback, 502 + Id));
+
+                //IsConnected = client.IsConnected;
                 _memoryMapFile = MemoryMappedFile.OpenExisting("SimInputData");
                 IsConnected = true;
             }
@@ -76,6 +82,10 @@ namespace EQX.InOut
 
         public override bool Disconnect()
         {
+            //client.Disconnect();
+
+            //IsConnected = client.IsConnected;
+            //return !IsConnected;
             _memoryMapFile?.Dispose();
             _memoryMapFile = null;
             IsConnected = false;
@@ -84,8 +94,10 @@ namespace EQX.InOut
 
         protected override bool ActualGetInput(int index)
         {
+            //var response = client.ReadCoils(0, 0, MaxPin);
             if (_memoryMapFile == null) return false;
 
+            //return (response[index / 8] & (0x01 << (index % 8))) != 0;
             using var stream = _memoryMapFile.CreateViewStream(index, 1);
             int value = stream.ReadByte();
             return value == 1;
