c6c7feabcff07139f850937adb24bf860dfe7436
Xuanhv21 <xuanhoangvinh01@gmail.com>
2025-09-20 11:22:25 +0700

    Add Simulation Analog Input Device

---
+++ EQX.InOut/InOut/Analog/SimulationAnalogInputDevice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.InOut.InOut.Analog
+{
+    public class SimulationAnalogInputDevice<TEnum> : AnalogInputDeviceBase<TEnum> where TEnum : Enum
+    {
+        public override bool IsConnected { get; protected set; }
+
+        public SimulationAnalogInputDevice()
+            : base()
+        {
+        }
+
+        public override bool Connect()
+        {
+            IsConnected = true;
+            return IsConnected;
+        }
+
+        public override bool Disconnect()
+        {
+            IsConnected = false;
+            return true;
+        }
+
+        public override double GetVolt(int channel)
+        {
+            Random rand = new Random();
+            return Math.Round(rand.NextDouble() * 10.0, 2); 
+        }
+        public override double GetCurrent(int channel)
+        {
+            Random rand = new Random();
+            return Math.Round(rand.NextDouble() * 20.0, 2); 
+        }
+    }
+}
