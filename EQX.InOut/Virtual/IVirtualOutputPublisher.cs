cbbcbccb8bd53a193fd851a93752595acbf24cab
NGUYEN PHU TRONG <trongong123@gmail.com>
2025-09-22 09:21:06 +0700

    Add Flag Simulation Window (ONOFF TrueFalse Flag)

---
+++ EQX.InOut/Virtual/IVirtualOutputPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQX.InOut.Virtual
+{
+    internal interface IVirtualOutputPublisher
+    {
+        void RegisterSubscriber(int outputPin, Action<bool> subscriber);
+    }
+}
---
+++ PIFilmFlagSimulationWindow/FlagDefinitions/EFixtureAlignProcessOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIFilmFlagSimulationWindow.FlagDefinitions
+{
+    public enum EFixtureAlignProcessOutput
+    {
+        FIXTURE_ALIGN_REQ_LOAD,
+        FIXTURE_ALIGN_DONE,
+
+        TRANSFER_FIXTURE_DONE_RECEIVED,
+    }
+}
