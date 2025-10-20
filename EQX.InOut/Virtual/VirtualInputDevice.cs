cb2661a12c33a0eea22506d5dd5e4bda626faf70
NGUYỄN HỮU DŨNG <nguyenhuudung.1310@gmail.com>
2025-10-20 10:12:14 +0700

    Update View

--- EQX.InOut/Virtual/VirtualInputDevice.cs
+++ EQX.InOut/Virtual/VirtualInputDevice.cs
@@ -14,107 +14,13 @@ namespace EQX.InOut.Virtual
 
         public void Mapping(int inputPin, IDOutputDevice outputDevice, int outputPin)
         {
-#if !SIMULATION
             _mappings.Add(inputPin, (outputDevice, outputPin));
-#else
-            if (outputDevice == null)
-            {
-                throw new ArgumentNullException(nameof(outputDevice));
-            }
-            int normalizedIndex = Normalize(inputPin);
-            _mappings[normalizedIndex] = (outputDevice, outputPin);
-
-            UpdateAutoValue(normalizedIndex, outputDevice[outputPin]);
-
-            if (outputDevice is IVirtualOutputPublisher publisher)
-            {
-                publisher.RegisterSubscriber(outputPin, value => UpdateAutoValue(normalizedIndex, value));
-            }
-#endif
         }
-#if SIMULATION
-        public void SetManualInput(int index, bool value)
-        {
-            int normalizedIndex = Normalize(index);
-            _manualOverrides[normalizedIndex] = value;
-            _sharedMemory?.SetManual(normalizedIndex, null);
-        }
-
-        public void ClearManualInput(int index)
-        {
-            int normalizedIndex = index % MaxPin;
-            _manualOverrides.Remove(normalizedIndex);
-        }
-
-        public void ClearManualInputs()
-        {
-            _manualOverrides.Clear();
-            _sharedMemory?.ClearManuals();
-        }
-
-        public bool HasManualInput(int index)
-        {
-            int normalizedIndex = Normalize(index);
-
-            if (_sharedMemory != null)
-            {
-                return _sharedMemory.HasManual(normalizedIndex);
-            }
-            return _manualOverrides.ContainsKey(normalizedIndex);
-        }
-#endif
-
 
         protected override bool ActualGetInput(int index)
         {
-#if !SIMULATION
             return _mappings.ContainsKey(index) && _mappings[index].outputDevice[_mappings[index].outputPin];
-#else
-            int normalizedIndex = Normalize(index);
-
-            if (_sharedMemory != null)
-            {
-                return _sharedMemory.GetEffectiveValue(normalizedIndex);
-            }
-
-            if (_manualOverrides.TryGetValue(normalizedIndex, out bool manualValue))
-            {
-                return manualValue;
-            }
-
-            if (_mappings.TryGetValue(normalizedIndex, out var mapping))
-            {
-                return mapping.outputDevice[mapping.outputPin];
-            }
-
-            return false;
-#endif
-
         }
-
-#if SIMULATION
-        private void UpdateAutoValue(int index, bool value)
-        {
-            _sharedMemory?.SetAutoValue(index, value);
-        }
-
-        private int Normalize(int index)
-        {
-            if (MaxPin <= 0)
-            {
-                return 0;
-            }
-
-            int normalized = index % MaxPin;
-            if (normalized < 0)
-            {
-                normalized += MaxPin;
-            }
-
-            return normalized;
-        }
-#endif
-
     }
 }
 
